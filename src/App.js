7fb8867ee0482b6c5a79a5a01f1387f7e18c93af
Tyler Morris Ford <tylerford@gmail.com>
2020-06-09 22:49:11 -0600

    add boilerplate + useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import axios from 'axios';
+export default function App() {
+  
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('react hooks');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const searchInputRef = useRef();
+
+  useEffect( () => {getResults()}, [])
+  
+      const getResults = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
+          setResults(response.data.hits);
+        } catch (err) {
+          setError(err)
+        }
+
+          setLoading(false);
+      }
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  }
+
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus()
+  }
+
+  return(
+    <div style={{display: 'grid', alignItems: 'center', justifyContent: 'center'}}> 
+    <h2 style={{textAlign: 'center'}}>Grab Articles From Hacker News</h2>
+    <div style={{display: 'inline-block', textAlign: 'center'}}>
+      <form onSubmit={handleSearch}>
+    <input style={{width: '300px'}} type='text' value={query} onChange={event => setQuery(event.target.value)} ref={searchInputRef}></input>
+    <button type='submit'>Search</button>
+    <button type='button' onClick={handleClearSearch}>Clear</button>
+      </form>
     </div>
-  );
+    {loading ? <div>Loading Results</div> : <ul>
+      {results.map(result => (
+        <li key={result.objectID}>
+          <a href={result.url}>{result.title}</a>
+        </li>
+      ))}
+    </ul>}
+    {error && <div>{error.message}</div>}
+    </div>
+  )
 }
-
-export default App;
